bb9ca5871e2e52f657600f420317e2d94ae9e0a9
NgZH-Buddy <2200690@sit.singaporetech.edu.sg>
2023-06-25 23:26:58 +0800

    future changes

--- buddy_test_windows.py
+++ buddy_test_windows.py
@@ -3,5 +3,5 @@ import os
 if os.name == 'nt':
     print("Windows OS found")
     
-    folder = os.path.join(os.path.join(os.environ['USERPROFILE']), 'folder_name')
+    folder = os.path.join(os.path.join(os.environ['USERPROFILE']), 'Downloads')
     print(folder)
